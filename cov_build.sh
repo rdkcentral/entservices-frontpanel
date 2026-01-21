e2c6298dba75d313409bde2eb545cb1e51450ab2
preeja33 <preeja_raveendran@comcast.com>
2026-01-21 20:51:44 +0530

    Update cov_build.sh

--- cov_build.sh
+++ cov_build.sh
@@ -6,7 +6,7 @@ GITHUB_WORKSPACE="${PWD}"
 ls -la ${GITHUB_WORKSPACE}
 ############################
 # Build entservices-frontpanel
-echo "buliding entservices-frontpanel"
+echo "building entservices-frontpanel"
 
 cd ${GITHUB_WORKSPACE}
 cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/entservices-frontpanel \
